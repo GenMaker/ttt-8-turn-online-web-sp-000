5a6d76be22518be3c284ddc6040c81cdef4b5fba
Panela Leung <leungpanela@gmail.com>
2019-08-04 14:12:08 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -33,9 +33,9 @@ def turn(board)
 
   user_input = gets.strip
 
-  input_to_index(user_input)
+  index = input_to_index(user_input)
 
-  valid_move?(board,user_input)
+  valid_move?(board,index)
 
 
 end
